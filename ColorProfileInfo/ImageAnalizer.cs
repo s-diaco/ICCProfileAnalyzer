d44d4c92ff548e4e136eea06de0dee1ee8a80069
Diaco - ink tester <s.diaco@live.com>
2017-03-06 08:34:32 +0330

    first commit

---
+++ ColorProfileInfo/ImageAnalizer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aurigma.GraphicsMill;
+using Aurigma.GraphicsMill.Transforms;
+using Aurigma.GraphicsMill.AdvancedDrawing;
+
+namespace ColorProfileInfo
+{
+    public class ImageAnalizer
+    {
+        public string GetChannelIds(System.IO.StreamReader sr)
+        {
+            System.IO.Stream stream = sr.BaseStream;
+
+            string retVal = "";
+            using (var bitmap = new Aurigma.GraphicsMill.Bitmap(stream))
+            {
+                for (int i = 0; i < bitmap.Channels.Count; i++)
+                {
+                    retVal += bitmap.Channels[i].Id + ",";
+                }
+                var cyanBitmap = bitmap.Channels[Aurigma.GraphicsMill.Channel.Cyan];
+                cyanBitmap.Save(@"Images\Output\Cmyk_test_Channel_C.tif");
+
+                var alphaBitmap = bitmap.Channels[Aurigma.GraphicsMill.Channel.Yellow];
+                alphaBitmap.Save(@"Images\Output\Cmyk_test_Channel_A.tif");
+            }
+            return retVal;
+        }
+        public string GetChannelsCount(System.IO.StreamReader sr)
+        {
+            System.IO.Stream stream = sr.BaseStream;
+            string retVal = "";
+            using (var bitmap = new Aurigma.GraphicsMill.Bitmap(stream))
+            {
+                retVal = bitmap.Channels.Count.ToString() + " Channels";
+            }
+            return retVal;
+        }
+    }
+}
---
+++ ICCProfileAnalyzer/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ColorProfileInfo;
+
+namespace ICCProfileAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                label1.Text = "";
+                label2.Text = "";
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                ImageAnalizer IA = new ImageAnalizer();
+                label1.Text = IA.GetChannelsCount(sr);
+                label2.Text = IA.GetChannelIds(sr);
+                sr.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var bitmap = new Aurigma.GraphicsMill.Bitmap(@"Images\cmyk.tif"))
+            {
+                label1.Text = "";
+                label1.Text = bitmap.Channels.Count.ToString() + " Channels";
+                label2.Text = "";
+                for (int i = 0; i < bitmap.Channels.Count; i++)
+                {
+                    label2.Text += bitmap.Channels[i].Id + ",";
+                }
+                var cyanBitmap = bitmap.Channels[Aurigma.GraphicsMill.Channel.Cyan];
+                cyanBitmap.Save(@"Images\Output\Cmyk_test_Channel_C.tif");
+
+                var alphaBitmap = bitmap.Channels[Aurigma.GraphicsMill.Channel.Yellow];
+                alphaBitmap.Save(@"Images\Output\Cmyk_test_Channel_A.tif");
+            }
+        }
+    }
+}
